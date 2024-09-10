0c6c8996ec620a391eca12908a70fda5bc698460
sangilish <56685007+sangilish@users.noreply.github.com>
2024-09-10 01:15:30 -0700

    complete 2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send('It works!');
+    res.render('form', { title: 'Registration form' });
 });
+
+router.get('/', function(req, res) {
+    res.render('form', { title: 'Registration form' });
+});
+
 
 module.exports = router;
