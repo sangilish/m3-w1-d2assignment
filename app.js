6638b96a4f11e1adb5df1ca8a514e49ad0255585
sangilish <56685007+sangilish@users.noreply.github.com>
2024-09-10 23:02:12 -0700

    assignment completed

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express =  require('express');
-const path = require('path');
-const routes = require('./routes/index');
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use('/', routes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", routes);
+
 
 module.exports = app;
